c4bce993650dd099a76b87a0660c0f4300f06edf
James Rostirolla <j_rostirolla@hotmail.com>
2021-06-18 10:25:15 +1000

    edit uploads - checkbox added to HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", ".", "+" ]
+var numericCharacters = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
+var alphaCharacters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+var capitalLetters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+//var trialOption = "abcdefghijklmnopqrstuvwxyz1234567890~`!@#$%^&*()-_=+.,"
+
+function startPrompt() {
+    var pwLength = prompt("How long would you like your password to be?", "Choose a num between 8 - 128");
+        console.log(pwLength);
+if (pwLength >= 8 && pwLength <= 128) {   
+    generatePassword()
+  } else {
+  alert("ATTENTION: Password length too short. Please pick a number between 8 and 128");
+}
+}
+
+
+
+var passwordArray = [ /*need to figure out how to select chosen arrays in here*/ ]
+
+//TODO: Right the loop
+function generatePassword() {
+  var lower = document.getElementById("lower").checked
+  var upper = document.getElementById("upper").checked
+  var number = document.getElementById("number").checked
+  var special = document.getElementById("special").checked
+
+  console.log(lower)
+  console.log(upper)
+  console.log(number)
+  console.log(special)
+
+  /*
+for (i = 0; i < startPrompt.pwLength; i++) {
+  //function getRandom() = getRandom(startPrompt.pwLength) ??
+  //insert combined array option here [Number from getRandom sequence here] ??
+  
+}*/
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -13,16 +52,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-/*
-TODO: solution ideas:
-- Generate Math.random several times, whichever number they select, to generate a random letter or number however many times they chose
-- 
-
-
-
-
-TODO: Create first prompt, length of PW, started when "generate PW button is pressed"
-
-TODO: 
-*/
+generatePassword()
